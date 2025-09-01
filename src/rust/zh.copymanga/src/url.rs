7b9994da56bbf9013eedb3baea56b152d8f5ac50
demo <demo@test.com>
2025-09-01 13:51:15 +0800

    update copymanga

--- src/rust/zh.copymanga/src/url.rs
+++ src/rust/zh.copymanga/src/url.rs
@@ -336,10 +336,13 @@ impl Url<'_> {
 	}
 
 	fn get(self) -> Request {
-		Request::get(self.to_string()).header(
-			"User-Agent",
-			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
-		)
+		Request::get(self.to_string())
+			.header(
+				"User-Agent",
+				"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+			)
+			.header("Dnts", "1")
+			.header("Referer", "https://webview.mfs.dpdns.org/")
 	}
 }
 
